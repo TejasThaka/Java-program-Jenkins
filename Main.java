54d94d219e77084b723e5258ac4969031af68e62
TejasThaka <126861384+TejasThaka@users.noreply.github.com>
2023-09-15 20:37:11 +0530

    Initial Commit

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         System.out.println("This is java programs");
         System.out.println("Output from java application");
         System.out.println("-------------");
+        System.out.println("Hello");
     }
 }
