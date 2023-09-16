7fa609da6d96f2301e5d5c87f1538b120e8f3890
TejasThaka <126861384+TejasThaka@users.noreply.github.com>
2023-09-16 19:24:35 +0530

    added student class

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Hello");
         System.out.println("----------------------");
         System.out.println("========================");
-        System.out.println("Hi this side tejas");
+        Student s = new Student();
+        s.sayHello();
     }
 }
---
+++ student.java
@@ -0,0 +1,8 @@
+public class Student {
+    System.out.println("this is tejas side");
+    }
+
+    public void sayHello() {
+        System.out.println("this side tejas..hii");
+    }
+}
